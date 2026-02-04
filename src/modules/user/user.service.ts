b536d8877bd708e0a205efc28b365553f91904ee
BitWizCoder <mhmdnoman01@gmail.com>
2026-02-04 16:44:27 +0600

    add login

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,6 @@
 import { prisma } from "../../lib/prisma";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const register = async (payload: any) => {
   const salt = await bcrypt.genSalt(10);
@@ -15,14 +16,32 @@ const register = async (payload: any) => {
   return user;
 };
 
-const login = async (payload: any) => {
+const login = async (payload: { email: string; password_hash: string }) => {
   const user = await prisma.user.findUnique({
-    where: {
-      email: payload.email,
-    },
+    where: { email: payload.email },
   });
 
-  return user;
+  if (!user) throw new Error("Invalid credentials");
+
+  const isMatch = await bcrypt.compare(
+    payload.password_hash,
+    user.password_hash,
+  );
+  if (!isMatch) throw new Error("Invalid credentials");
+
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
+  const token = jwt.sign({ email: user.email }, secret, { expiresIn: "1h" });
+
+  return {
+    message: "Login successful",
+    token,
+    user: { id: user.id, email: user.email },
+  };
 };
 
 export const userService = {
